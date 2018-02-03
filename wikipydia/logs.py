7c35d9882931f7bcda074ceac3da249810fdaa1e
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-02-03 16:03:40 +0000

    why is there a space there

--- wikipydia/logs.py
+++ wikipydia/logs.py
@@ -1,12 +1,11 @@
-
 def info(msg):
-    print(f'[i] {msg}')
-
+    print(f'[Info] {msg}')
+    
 def warn(msg):
     print(f'[Warning] {msg}')
 
 def err(msg):
-    print(f'[Error!] {msg}')
+    print(f'[Error] {msg}')
 
 def crit(msg):
     print(f'[CRITICAL ERROR] {msg}')
